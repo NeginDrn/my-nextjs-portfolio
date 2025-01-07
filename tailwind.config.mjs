91d9ea1d70f42e7b9377d82f57844c5cffb83f96
Negindn88 <110387634+Negindn88@users.noreply.github.com>
2025-01-07 15:29:17 +0000

    Initial commit: Set up Next.js project with Tailwind CSS and JetBrains Mono

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,17 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: ["class"],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "150px",
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "960px",
+        xl: "1200px"
+      }
+    },
     extend: {
+      fontFamily: {
+        primary: "var(--font-jetbrainsMono)"
+      },
       colors: {
         background: "var(--background)",
-        foreground: "var(--foreground)",
+        foreground: "var(--foreground)"
       },
-    },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)"
+      }
+    }
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")]
 };
