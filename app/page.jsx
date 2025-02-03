3e9ada64774f32149416431592ea84db9d475061
Negindn88 <110387634+Negindn88@users.noreply.github.com>
2025-02-03 17:16:19 +0000

    feat: add animated Photo component with Framer Motion

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,7 @@ import { Button } from "@/components/ui/button";
 import { FiDownload } from "react-icons/fi";
 // components
 import Social from "@/components/Social";
+import { Photo } from "@/components/Photo";
 
 const Home = () => {
   return (
@@ -9,7 +10,7 @@ const Home = () => {
       <div className="container mx-auto h-full">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
           {/* text */}
-          <div className="text-center xl:text-left">
+          <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Software Engineer</span>
             <h1 className="h1 mb-6">
               Hello I'm
@@ -40,7 +41,9 @@ const Home = () => {
             </div>
           </div>
           {/* image */}
-          <div>photo</div>
+          <div className="order-1 xl:order-none mb-8 xl:mb-0">
+            <Photo />
+          </div>
         </div>
       </div>
     </section>
